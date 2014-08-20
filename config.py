d896e3bfe949859a7c7dea58c81df05fc18f1d95
Mackenzie Sweeney <mackenzie.sweeney@gmail.com>
2014-08-20 18:42:47 -0400

    add additional options to config file

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import os
 
 SIZE_TRAINING_DATA = 45840617
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+MAPPER_DIR = os.path.join(PROJECT_DIR, 'mappers')
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
+
 TRAINING_DATA_FILE = os.path.join(DATA_DIR, 'train.csv')
 TEST_DATA_FILE = os.path.join(DATA_DIR, 'test.csv')
 ZIPTRAIN = os.path.join(DATA_DIR, 'train.zip')
