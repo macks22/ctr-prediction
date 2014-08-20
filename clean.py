ed4813bdc098bf24122c250daa5998cdef29420b
Mackenzie Sweeney <mackenzie.sweeney@gmail.com>
2014-08-20 18:41:33 -0400

    add scaling to missing value fill

--- clean.py
+++ clean.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import pandas as pd
+from sklearn import preprocessing
 
 
 int_cols = ['I{}'.format(num) for num in range(1, 14)]
@@ -10,9 +11,13 @@ all_cols = int_cols + multivar_cols
 
 
 def fill(df):
+    """Fill in missing values and perform feature scaling."""
+
     # fill with median values for integer features
     for col in int_cols:
+        df[col] = df[col].astype('float')
         df[col][df[col].isnull()] = df[col].median()
+        df[col] = preprocessing.scale(df[col])
 
     # map all multivariate features to integers, then fill with median
     for col in multivar_cols:
@@ -22,10 +27,11 @@ def fill(df):
         mapper = dict(zip(distinct_vals, range(len(distinct_vals))))
         df[col] = column.map(mapper)
         df[col][column == -1] = df[col].median()
+        df[col] = preprocessing.scale(df[col].astype('float'))
 
 
-def labels_data(df):
-    labels = df['Label'].values
+def labels_data(df, id_label='Label'):
+    labels = df[id_label].values
     data = df[all_cols].values
     return labels, data
 
