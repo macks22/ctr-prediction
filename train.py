037d5399ebb91b519b5df77544b6eb480a807749
Mackenzie Sweeney <mackenzie.sweeney@gmail.com>
2014-08-20 18:42:32 -0400

    changed model to regressor using least squares

--- train.py
+++ train.py
@@ -2,13 +2,14 @@ import sys
 import cPickle as pickle
 import zipfile
 
-import sklearn.linear_model as sklin
 import pandas as pd
+import numpy as np
+import sklearn.linear_model as sklin
 
 import clean
 
 
-def iterdata(archive_path):
+def iterdata(archive):
     """Incrementally yield pre-processed data files from a zip archive. This
     assumes each file in the archive can be read into memory.
 
@@ -28,19 +29,28 @@ def iterdata(archive_path):
 if __name__ == "__main__":
     zippath = sys.argv[1]
     datagen = iterdata(zippath)
-    lreg = sklin.SGDClassifier(penalty='l1', loss='log')
+    lreg = sklin.SGDRegressor(penalty='l1', loss='squared_loss', verbose=1)
     classes = [0, 1]
 
-    num_files = 111  # should get this for real later
-    num_train = 80
-    num_test = num_files - num_train
+    with zipfile.ZipFile(zippath) as archive:
+        num_files = len(archive.filelist)
+        num_train = np.ceil((2/3) * num_files)
+        num_test = num_files - num_train
+
+    MB = 1024*1024
+    # this estimation is from the sklearn docs
+    lreg.n_iter = np.ceil(10**6 / (num_train * 100*MB))
+    if lreg.n_iter < 5:
+        lreg.n_iter = 5
 
     # train the model
     print 'training logistic regression on {} files'.format(num_train)
+    print 'minimizing loss function: {}'.format(lreg.loss.replace('_', ' '))
     for num in range(num_train):
         print 'file num: {}'.format(num)
         labels, data = clean.labels_data(datagen.next())
-        lreg.partial_fit(data, labels, classes)
+        lreg.partial_fit(data, labels)
+        # lreg.partial_fit(data, labels, classes)
 
     # combine all test data
     testdf = datagen.next()
@@ -49,5 +59,5 @@ if __name__ == "__main__":
 
     testlabels, testdata = clean.labels_data(testdf)
     print lreg.score(testdata, testlabels)
-    with open('logreg.pickle', 'w') as f:
+    with open('logreg-ls.pickle', 'w') as f:
         pickle.dump(lreg, f)
