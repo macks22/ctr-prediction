27a7f88f07a3b17db0885934648315e779ea1346
Mackenzie Sweeney <mackenzie.sweeney@gmail.com>
2014-08-19 20:13:39 -0400

    update csv splitting scripts

--- csvsplit.py
+++ csvsplit.py
@@ -1,25 +1,26 @@
 import os
 import sys
+import time
+import logging
 
 
 MB = 1024*1024
 MB100 = 100*MB
 
 
-def iterchunk(fpath, block_size=MB100):
-    with open(fpath, 'r') as f:
-        lines = []
-        size = 0
-        for line in f:
-            size += len(line)
-            lines.append(line)
-            if size > block_size:
-                yield lines
-                lines = []
-                size = 0
+def iterchunk(f, block_size=MB100):
+    lines = []
+    size = 0
+    for line in f:
+        size += len(line)
+        lines.append(line)
+        if size > block_size:
+            yield lines
+            lines = []
+            size = 0
 
-        # return final chunk
-        yield lines
+    # return final chunk
+    yield lines
 
 
 def iterprefix(cap):
@@ -36,7 +37,7 @@ if __name__ == "__main__":
     # usage check
     if len(sys.argv) < 2:
         prog = os.path.basename(__file__)
-        usage = '{} <csvfile> [<outdir>]'.format(PROG)
+        usage = '{} <csvfile> [<outdir>] [<block-size>]'.format(prog)
         print usage
         sys.exit(1)
 
@@ -52,9 +53,12 @@ if __name__ == "__main__":
 
     with open(csvfile, 'r') as source:
         headers = source.readline()
+        print 'output directory: {}'.format(outdir)
         for chunk in iterchunk(source, block_size):
             prefix = prefixgen.next()
-            path = os.path.join(outdir, '{}-split.csv'.format(prefix))
+            fname = '{}-split.csv'.format(prefix)
+            path = os.path.join(outdir, fname)
+            print 'writing {} bytes to: {}'.format(block_size, fname)
             with open(path, 'w') as dest:
                 dest.write(headers)
                 dest.writelines(chunk)
